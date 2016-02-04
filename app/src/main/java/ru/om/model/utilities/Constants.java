0a14d9037f96acfb5db837cf32542e0582c72a06
nskomega <2588000nsk@gmail.com>
2016-02-04 12:24:07 +0600

    add url YOUTUBE

--- app/src/main/java/ru/om/model/utilities/Constants.java
+++ app/src/main/java/ru/om/model/utilities/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String BASE_URL = "http://json.ucoz.net";
     public static final String PHOTO_URL_1 = "http://img.youtube.com/vi/";
     public static final String PHOTO_URL_2 = "/maxresdefault.jpg";
+    public static final String YOUTUBE = "https://www.youtube.com/watch?v=";
 }
